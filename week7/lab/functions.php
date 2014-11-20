9d5646e534cbdf115b10874bea69bb2a993a9c31
001337436 <001337436@N242D1.labs.neit.edu>
2014-11-19 20:13:23 -0500

    week 7 lab

---
+++ week7/lab/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+*check log in - function that will take an email and password and confirm that it
+    is found in the database.
+*check if email exist  - function to check if an email exist in the database
+*valid email - function to check if an email is valid
+*valid password - function to check if a password is valid
+*/
+function checkLogin()
+{
+    
+}
+
+function checkEmail()
+{
+    
+}
+
+function validateEmail()
+{
+    
+}
+
+function validatePassword()
+{
+    
+}
