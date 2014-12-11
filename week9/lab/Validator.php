76b025c8955205b6f3f478c369b89b91b00ac208
001337436 <001337436@N242D2.labs.neit.edu>
2014-12-10 19:22:47 -0500

    week9 lab

---
+++ week9/lab/Validator.php
@@ -0,0 +1,79 @@
+<?php
+
+class Validator
+{
+    public function checkEmail($email)
+    {
+            $db = new PDO("mysql:host=localhost;dbname=phpclassfall2014", "root", "");
+            $dbs = $db->prepare("select email from signup where email = :email");
+            $dbs->bindParam(':email', $email);
+
+            if ( $dbs->execute()  && $dbs->rowCount() > 0) 
+            {	
+                    return true;
+            }
+            else
+            {
+                    return false;
+            }
+    }
+
+    public function checkPassword($password)
+    {
+            $password = sha1($password);
+            $db = new PDO("mysql:host=localhost;dbname=phpclassfall2014", "root", "");
+            $dbs = $db->prepare("select password from signup where password = :password");
+            $dbs->bindParam(':password', $password);
+
+            if ( $dbs->execute()  && $dbs->rowCount() > 0) 
+            {
+                    return true;
+            }
+            else
+            {
+                    return false;
+            }
+    }
+
+    public function checkLogin($email, $password)
+    {
+            $password = sha1($password);
+            $db = new PDO("mysql:host=localhost;dbname=phpclassfall2014", "root", "");
+            $dbs = $db->prepare("select email, password from signup where password = :password and email = :email");
+            $dbs->bindParam(':password', $password);
+            $dbs->bindParam(':email', $email);
+
+            if ( $dbs->execute()  && $dbs->rowCount() > 0) 
+            {
+                    return true;
+            }
+            else
+            {
+                    return false;
+            }
+    }
+
+    public function validateEmail($email)
+    {
+         if ( filter_var($email, FILTER_VALIDATE_EMAIL) === false )
+            {
+                    return false;
+            }
+            else
+            {
+                    return true;
+            }
+    }
+
+    public function validatePassword($password)
+    {
+            if ( strlen($password) < 4 )
+            {
+                    return false;
+            }
+            else
+            {
+                    return true;
+            }
+    }
+}
